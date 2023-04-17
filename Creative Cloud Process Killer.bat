4a2c6bf505f4abb023020f813332f1c2c491c867
athkiasaris1 <53872050+athkiasaris1@users.noreply.github.com>
2023-04-17 15:18:53 +0300

    Upload

--- Creative Cloud Process Killer.bat
+++ Creative Cloud Process Killer.bat
@@ -19,44 +19,7 @@ exit
 )
 if "%~1"=="-killcc" (
  if "%~2"=="" (
- title Creative Cloud Process Killer
- cls
- echo[
- echo  Adobe Creative Cloud ^(CC^) Process Killer
- echo[
- echo  WARNING This script will JUST kill the processes
- echo  meaning YOU HAVE TO run it everytime you boot 
- echo  your computer! Use the Stop Creative Cloud on
- echo  start up tool on the main menu to stop it from 
- echo  start up
- echo[
- echo FOR THE PROCESS "AdobeUpdateService.exe" to get
- echo TERMINATED you NEED TO RUN THE SCRIPT AS AN
- echo ADMINISTRATOR!
- echo[
- pause
- echo[
- taskkill /F /im CClibrary.exe
- taskkill /F /im CCXProcess.exe
- taskkill /F /im CoreSync.exe
- taskkill /F /im "Creative Cloud.exe"
- taskkill /F /im "Creative Cloud Helper.exe"
- taskkill /F /im "Adobe Desktop Service.exe"
- taskkill /F /im AdobeNotificationClient.exe
- taskkill /F /im AdobeIPCBroker.exe
- taskkill /F /im "Adobe CEF Helper.exe"
- taskkill /F /im "adobe_licensing_wf.exe"
- taskkill /F /im "AdobeUpdateService.exe"
- echo[
- echo DONE!
- echo[
- echo If it said success this tool worked!!!
- echo[
- echo If it said "ERROR The process was not found"
- echo The processes were already killed or you 
- echo need to run this script as an administrator
- echo to kill the processes
- echo[
+ call :ProcessKiller
  pause
  exit
  )
@@ -77,26 +40,7 @@ if "%~1"=="-killcc" (
 )
 if "%~1"=="-stopautocc" (
  if "%~2"=="" (
- title Start Up Stopper ^> Creative Cloud Process Killer
- cls
- echo[
- echo  Creative Cloud Start Up Stopper
- echo[
- echo   WARNING If it says ACCESS DENIED
- echo   You need to RUN this Script as
- echo   an administrator!
- echo[
- echo   WARNING 2 IF YOU WANT NONE OF THE
- echo   CC PROCESSES RUNNING PLEASE 
- echo   DISABLE CCXProcess.exe FROM TASK
- echo   MANAGER START UP
- echo[
- pause
- echo[
- schtasks.exe /Change /TN "Adobe Creative Cloud" /Disable
- echo[
- echo DONE!
- echo[
+ call :StartupStopper
  pause
  exit
  )
@@ -107,45 +51,7 @@ if "%~1"=="-stopautocc" (
 )
 
 if "%~1"=="-updates" (
-title Updates ^> Creative Cloud Stopper Killer
-cls
-echo[
-echo  Checking The Ineternet Connection
-Ping www.google.com -n 1 -w 1000
-cls
-if errorlevel 1 (
-echo[
-echo            --------------------------
-echo           ^|                          ^|
-echo           ^|     Check for Updates    ^|
-echo           ^|                          ^|
-echo           ^|    Your Version: %version%   ^|
-echo           ^|                          ^|
-echo           ^|   Please Connect to the  ^|
-echo           ^|   Internet to Check for  ^|
-echo           ^|         Updates          ^|
-echo           ^|                          ^|
-echo            --------------------------
-echo[
-echo[
-) else (
-echo[
-echo            --------------------------
-echo           ^|                          ^|
-echo           ^|     Check for Updates    ^|
-echo           ^|                          ^|
-echo           ^|   Your Version: %version%    ^|
-echo           ^|                          ^|
-curl https://athkiasaris1.github.io/CreativeCloudProcessKiller-web/1.7.1+/currentversionwithlines.html
-curl https://athkiasaris1.github.io/CreativeCloudProcessKiller-web/1.7.1+/currentprereleasewithlines.html
-echo           ^|                          ^|
-echo           ^|    To Update go to the   ^|
-echo           ^|          Github          ^|
-echo           ^|                          ^|
-echo            --------------------------
-echo[
-echo[
-)
+ call :CheckUpdates
  pause
  exit
 )
@@ -255,45 +161,7 @@ goto menu
 )
 if errorlevel 5 exit
 if errorlevel 4 (
-title Updates ^> Creative Cloud Stopper Killer
-cls
-echo[
-echo  Checking The Ineternet Connection
-Ping www.google.com -n 1 -w 1000
-cls
-if errorlevel 1 (
-echo[
-echo            --------------------------
-echo           ^|                          ^|
-echo           ^|     Check for Updates    ^|
-echo           ^|                          ^|
-echo           ^|    Your Version: %version%   ^|
-echo           ^|                          ^|
-echo           ^|   Please Connect to the  ^|
-echo           ^|   Internet to Check for  ^|
-echo           ^|         Updates          ^|
-echo           ^|                          ^|
-echo            --------------------------
-echo[
-echo[
-) else (
-echo[
-echo            --------------------------
-echo           ^|                          ^|
-echo           ^|     Check for Updates    ^|
-echo           ^|                          ^|
-echo           ^|   Your Version: %version%    ^|
-echo           ^|                          ^|
-curl https://athkiasaris1.github.io/CreativeCloudProcessKiller-web/1.7.1+/currentversionwithlines.html
-curl https://athkiasaris1.github.io/CreativeCloudProcessKiller-web/1.7.1+/currentprereleasewithlines.html
-echo           ^|                          ^|
-echo           ^|    To Update go to the   ^|
-echo           ^|          Github          ^|
-echo           ^|                          ^|
-echo            --------------------------
-echo[
-echo[
-)
+call :CheckUpdates
 pause
 cls
 goto menu
@@ -335,39 +203,19 @@ cls
 goto menu
 )
 if errorlevel 2 (
-title Start Up Stopper ^> Creative Cloud Process Killer
-cls
-echo             _____  _____ ______ _   __
-echo            /  __ \/  __ \^| ___ \ ^| / /
-echo            ^| /  \/^| /  \/^| ^|_/ / ^|/ / 
-echo            ^| ^|    ^| ^|    ^|  __/^|    \
-echo            ^| \__/\^| \__/\^| ^|   ^| ^|\  \
-echo             \____/ \____/\_^|   \_^| \_/
-echo           Creative Cloud Process Killer
-echo --------------------------------------------------
-echo[
-echo  Creative Cloud Start Up Stopper
-echo[
-echo   WARNING If it says ACCESS DENIED
-echo   You need to RUN this Script as
-echo   an administrator!
-echo[
-echo   WARNING 2 IF YOU WANT NONE OF THE
-echo   CC PROCESSES RUNNING PLEASE 
-echo   DISABLE CCXProcess.exe FROM TASK
-echo   MANAGER START UP MENU
-echo[
-pause
-echo[
-schtasks.exe /Change /TN "Adobe Creative Cloud" /Disable
-echo[
-echo DONE!
-echo[
+call :StartupStopper
 pause
 cls
 goto menu
 )
 if errorlevel 1 (
+call :ProcessKiller
+pause
+cls
+goto menu
+)
+
+:ProcessKiller
 title Creative Cloud Process Killer
 cls
 echo             _____  _____ ______ _   __
@@ -414,7 +262,77 @@ echo The processes were already killed or you
 echo need to run this script as an administrator
 echo to kill the processes
 echo[
-pause
+goto :EOF
+
+:StartupStopper
+title Start Up Stopper ^> Creative Cloud Process Killer
 cls
-goto menu
+echo             _____  _____ ______ _   __
+echo            /  __ \/  __ \^| ___ \ ^| / /
+echo            ^| /  \/^| /  \/^| ^|_/ / ^|/ / 
+echo            ^| ^|    ^| ^|    ^|  __/^|    \
+echo            ^| \__/\^| \__/\^| ^|   ^| ^|\  \
+echo             \____/ \____/\_^|   \_^| \_/
+echo           Creative Cloud Process Killer
+echo --------------------------------------------------
+echo[
+echo  Creative Cloud Start Up Stopper
+echo[
+echo   WARNING If it says ACCESS DENIED
+echo   You need to RUN this Script as
+echo   an administrator!
+echo[
+echo   WARNING 2 IF YOU WANT NONE OF THE
+echo   CC PROCESSES RUNNING PLEASE 
+echo   DISABLE CCXProcess.exe FROM TASK
+echo   MANAGER START UP MENU
+echo[
+pause
+echo[
+schtasks.exe /Change /TN "Adobe Creative Cloud" /Disable
+echo[
+echo DONE!
+echo[
+goto :EOF
+
+:CheckUpdates
+title Updates ^> Creative Cloud Stopper Killer
+cls
+echo[
+echo  Checking The Ineternet Connection
+Ping www.google.com -n 1 -w 1000
+cls
+if errorlevel 1 (
+echo[
+echo            --------------------------
+echo           ^|                          ^|
+echo           ^|     Check for Updates    ^|
+echo           ^|                          ^|
+echo           ^|    Your Version: %version%   ^|
+echo           ^|                          ^|
+echo           ^|   Please Connect to the  ^|
+echo           ^|   Internet to Check for  ^|
+echo           ^|         Updates          ^|
+echo           ^|                          ^|
+echo            --------------------------
+echo[
+echo[
+) else (
+echo[
+echo            --------------------------
+echo           ^|                          ^|
+echo           ^|     Check for Updates    ^|
+echo           ^|                          ^|
+echo           ^|   Your Version: %version%    ^|
+echo           ^|                          ^|
+curl https://athkiasaris1.github.io/CreativeCloudProcessKiller-web/1.7.1+/currentversionwithlines.html
+curl https://athkiasaris1.github.io/CreativeCloudProcessKiller-web/1.7.1+/currentprereleasewithlines.html
+echo           ^|                          ^|
+echo           ^|    To Update go to the   ^|
+echo           ^|          Github          ^|
+echo           ^|                          ^|
+echo            --------------------------
+echo[
+echo[
 )
+goto :EOF
